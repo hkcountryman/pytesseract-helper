28c34ec04a76c113c3f182663480d63d70ff7bc7
Halle Countryman <hkcountryman@gmail.com>
2021-01-16 16:23:01 -0800

    Just starting

---
+++ img_to_txt.py
@@ -0,0 +1,15 @@
+import cv2
+from PIL import Image
+import PIL.Image
+from pytesseract import image_to_string
+import pytesseract
+
+# On Windows, you need to tell it where Tesseract is installed, for example:
+# pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe
+
+def to_text(pic):
+    img = Image.open(pic)
+    text = pytesseract.image_to_string(img)
+    return text
+
+to_text(test.jpg)
